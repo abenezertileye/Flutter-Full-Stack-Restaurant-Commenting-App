14c24ba1ead114c6b4db13dd034559c6401e0c42
Fevi-lendu <150247854+Fevi-lendu@users.noreply.github.com>
2024-05-31 17:09:51 -0700

    fev-storage

--- restaurant_review/lib/data/storage.dart
+++ restaurant_review/lib/data/storage.dart
@@ -1,4 +1,3 @@
-//storage.dart
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorage {
@@ -9,17 +8,19 @@ class SecureStorage {
   static final SecureStorage instance = SecureStorage._privateConstructor();
 
   final FlutterSecureStorage _storage = FlutterSecureStorage();
-
+//STORES TOKEN
   Future<void> write(String key, value) async {
     print("writing to storage...");
     print("$key : $value");
     await _storage.write(key: key, value: value);
   }
 
+//READS TOKEN
   Future<String?> read(key) async {
     return await _storage.read(key: key);
   }
 
+//DELETES TOKEN
   Future<void> delete(key) async {
     await _storage.delete(key: key);
   }
