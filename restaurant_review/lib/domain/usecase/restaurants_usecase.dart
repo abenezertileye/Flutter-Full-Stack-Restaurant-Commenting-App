9c3911d9c982915009afe676ff297d3bf6016dbc
Fevi-lendu <150247854+Fevi-lendu@users.noreply.github.com>
2024-05-31 17:10:29 -0700

    fec - restaurant usecase

--- restaurant_review/lib/domain/usecase/restaurants_usecase.dart
+++ restaurant_review/lib/domain/usecase/restaurants_usecase.dart
@@ -5,7 +5,7 @@ class RestaurantsUseCase {
   final RestaurantsRepository restaurantsRepository;
 
   RestaurantsUseCase({required this.restaurantsRepository});
-
+//REDIREECTS TO RESTAURANT REPOSITORY TO FETCH ALL RESTAURANTS
   Future<List<dynamic>> execute() async {
     final user = await restaurantsRepository.FetchRestaurants();
     // print(user);
